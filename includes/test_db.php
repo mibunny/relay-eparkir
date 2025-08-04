d9c4d477d74d5596e76f65b113103172cc0af8c8
mibunny <64744016+mibunny@users.noreply.github.com>
2025-08-05 02:43:21 +0700

    Create test_db.php

---
+++ includes/test_db.php
@@ -0,0 +1,15 @@
+<?php
+$host = "mysql.railway.internal";
+$db   = "railway";
+$user = "root";
+$pass = "yDtVxAAwXAacKARCVpUaejJyEcgFSzZC";
+$port = 3306;
+
+try {
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "✅ Koneksi berhasil ke database Railway!";
+} catch(PDOException $e) {
+    echo "❌ Koneksi gagal: " . $e->getMessage();
+}
+?>
